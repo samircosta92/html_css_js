d3a0ec27ed3be4411756fe0d5d130328a0885308
Samir Costa <65731013+samircosta92@users.noreply.github.com>
2021-05-12 09:47:31 -0300

    Add files via upload

---
+++ 2CAW/Trabalhos/AV2/Scripts/canvas.js
@@ -0,0 +1,10 @@
+var canvas = document.getElementById("ret");
+var context = canvas.getContext("2d")
+context.fillStyle = "rgb(0,0,200)";
+context.fillRect(10, 0, 40, 40);
+context.fillStyle = "rgb(200,0,0, 0.5)";
+context.fillRect(30, 20, 40, 40);
+context.fillStyle = "rgb(0,0,200)";
+context.fillRect(1200, 0, 40, 40);
+context.fillStyle = "rgb(200,0,0, 0.5)";
+context.fillRect(1180, 20, 40, 40);
